e588c59ca6195b63c23f6a38feefeda090616096
sneha.kolagani <snehakolagani@gmail.com>
2025-01-06 20:40:02 +0530

    created tore

--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { UserProvider } from './userContext';
+import { Provider } from 'react-redux';
+import store from './redux/Store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
   <React.StrictMode>
+    <Provider store={store}>
      <UserProvider>
       <App />
     </UserProvider>
+    </Provider>
   </React.StrictMode>
 );
 
